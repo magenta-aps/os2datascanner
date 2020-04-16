5f58d7aaa918835e02944a7148b20e480964c4e1
Steffen Jørgensen <sj@magenta-aps.dk>
2020-04-16 15:19:46 +0200

    PoC of match handling

--- src/os2datascanner/projects/report/reportapp/static/src/js/index.js
+++ src/os2datascanner/projects/report/reportapp/static/src/js/index.js
@@ -2,3 +2,33 @@ import '../css/master.scss';
 
 // Copy Path function
 new ClipboardJS(document.querySelectorAll('[data-clipboard-text]'));
+
+// Handle matches, start
+const button = $('.handle-match-button');
+const buttonWidth = button.outerWidth(false);
+const tooltip = $('.handle-match');
+
+var offsetTop = 10
+
+$(tooltip).css({
+  marginTop: offsetTop,
+  marginLeft: -buttonWidth/2
+});
+
+$(button).click(function() {
+  console.log("click!");
+
+  $(this).toggleClass("is-active");
+  $(this).siblings(tooltip).toggleClass("show");
+})
+
+function openMatchHandler() {
+  console.log("openMatchHandler");
+  $(tooltip).addClass("show");
+}
+
+function closeMatchHandler() {
+  console.log("closeMatchHandler");
+  $(tooltip).removeClass("show");
+}
+// Handle matches, stop
