54b24fbf3a9756d209ef56274751578c9e9deafa
Steffen Jørgensen <sj@magenta-aps.dk>
2020-04-23 09:34:08 +0200

    Cleaned up the scanner delete function + Added JS window.confirm() for both rules and scanners

--- src/os2datascanner/projects/admin/adminapp/views/scanner_views.py
+++ src/os2datascanner/projects/admin/adminapp/views/scanner_views.py
@@ -179,9 +179,6 @@ class ScannerUpdate(ScannerBase, RestrictedUpdateView):
 
 
 class ScannerDelete(RestrictedDeleteView):
-    """Delete a scanner view."""
-    template_name = 'os2datascanner/scanners.html'
-
     def get_form(self, form_class=None):
         """Adds special field password and decrypts password."""
         if form_class is None:
