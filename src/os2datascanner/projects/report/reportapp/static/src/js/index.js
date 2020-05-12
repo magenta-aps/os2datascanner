99774938413fc8f5a6727734146c2659db438b9b
Alexander Faithfull <af@magenta.dk>
2020-05-12 12:53:45 +0200

    [#35394] Added a simple report module API client to index.js
    This is really simple and really stupid, but it's a start

--- src/os2datascanner/projects/report/reportapp/static/src/js/index.js
+++ src/os2datascanner/projects/report/reportapp/static/src/js/index.js
@@ -5,6 +5,7 @@ new ClipboardJS(document.querySelectorAll('[data-clipboard-text]'));
 
 // Handle matches, start
 const button = $('.handle-match-button');
+const actions = $('.handle-match__action')
 const buttonWidth = button.outerWidth(false);
 const tooltip = $('.handle-match');
 
@@ -18,6 +19,57 @@ $(tooltip).css({
 $(button).click(function() {
   $(this).toggleClass("is-active");
   $(this).siblings(tooltip).toggleClass("show");
+})
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+$(actions).click(function() {
+  const button = $(this)
+
+  const report_id = Number.parseInt(button.attr("data-report-pk"))
+  const new_status = Number.parseInt(button.attr("data-status"))
+
+  if (!Number.isNaN(report_id) && !Number.isNaN(new_status)) {
+    $.ajax({
+      url: "/api",
+      method: "POST",
+      data: JSON.stringify({
+        "action": "set-status-1",
+
+        "report_id": report_id,
+        "new_status": new_status
+      }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      beforeSend: function(xhr, settings) {
+        xhr.data = JSON.stringify(xhr.data)
+        xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"))
+      }
+    }).done(function(body) {
+      status = body["status"]
+      if (status == "ok") {
+        button.closest("tr").fadeOut()
+      } else if (status == "fail") {
+        console.log(
+            "Attempt to call set-status-1 failed: "
+            + body["message"])
+      }
+    })
+  }
 })
 
 function openMatchHandler() {
