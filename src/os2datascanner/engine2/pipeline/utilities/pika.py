dadc32d324839231843b1ef0c1f6398c209d3c6a
Mikkel Rostved <mr@magenta-aps.dk>
2020-12-17 14:40:43 +0100

    [#40159] Run jobs in separate thread to make room for heartbeats. Ack when done in main thread.

--- src/os2datascanner/engine2/pipeline/utilities/pika.py
+++ src/os2datascanner/engine2/pipeline/utilities/pika.py
@@ -1,12 +1,20 @@
 from abc import ABC, abstractmethod
-from sys import stderr
+from sys import stderr, stdout
 import json
 import pika
+import functools
+import threading
+import logging
 
 from ...utilities.backoff import run_with_backoff
 from ....utils.system_utilities import json_utf8_decode
 from os2datascanner.utils import pika_settings
 
+logger = logging.getLogger(__name__)
+# TODO: change this to something configurable
+logger.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler(stdout)
+logger.addHandler(consoleHandler)
 
 class PikaConnectionHolder(ABC):
     """A PikaConnectionHolder manages a blocking connection to a RabbitMQ
@@ -23,7 +31,7 @@ class PikaConnectionHolder(ABC):
 
     def make_connection(self):
         """Constructs a new Pika connection."""
-        conn_string_tpl = '{0}://{1}:{2}@{3}:{4}/{5}?heartbeat=6000'
+        conn_string_tpl = '{0}://{1}:{2}@{3}:{4}/{5}?heartbeat=15'
         conn_string = conn_string_tpl.format(pika_settings.AMQP_SCHEME, 
                                              pika_settings.AMQP_USER, 
                                              pika_settings.AMQP_PWD, 
@@ -124,6 +132,7 @@ class PikaPipelineRunner(PikaConnectionHolder):
             self._pending = self._pending[1:]
 
     def publish_message(self, routing_key, message):
+        logger.debug("publish_message: %d" % threading.get_ident())
         self.channel.basic_publish(
                 exchange='',
                 routing_key=routing_key,
@@ -134,7 +143,15 @@ class PikaPipelineRunner(PikaConnectionHolder):
         faults in the Pika loop are silently handled without dropping any
         messages."""
 
-        def _queue_callback(channel, method, properties, body):
+        def _ack_message(channel, delivery_tag):
+            logger.debug("_ack_message: %d" % threading.get_ident())
+            if channel.is_open:
+                channel.basic_ack(delivery_tag)
+            else:
+                logger.debug("_ack_message: could not ack - channel closed")
+                pass
+
+        def _queue_callback(connection, channel, method, body):
             """Handles an AMQP message by calling the handle_message function
             and sending everything that it yields as a new message.
 
@@ -143,28 +160,54 @@ class PikaPipelineRunner(PikaConnectionHolder):
             connection is closed), then this function will continue to collect
             yielded messages and will schedule them to be sent when the
             connection is reopened."""
-            channel.basic_ack(method.delivery_tag)
-            self.dispatch_pending(expected=0)
-            decoded_body = json_utf8_decode(body)
-            if decoded_body:
-                failed = False
-                for routing_key, message in self.handle_message(
-                        decoded_body, channel=method.routing_key):
-                    # Try to dispatch messages as soon as they're generated,
-                    # but store them for later if the connection is dropped
-                    self._pending.append((routing_key, message))
-                    if not failed:
-                        try:
-                            self.dispatch_pending(expected=1)
-                        except RECOVERABLE_PIKA_ERRORS:
-                            failed = True
+
+            logger.debug("_queue_callback: %d" % threading.get_ident())
+
+            # Request a call to dispatch_pending as soon as possible in the context of the connection’s thread
+            dispatch_callback = functools.partial(self.dispatch_pending, expected=0)
+            connection.add_callback_threadsafe(dispatch_callback)
+
+            try:
+                decoded_body = json_utf8_decode(body)
+                if decoded_body:
+                    failed = False
+                    for routing_key, message in self.handle_message(
+                            decoded_body, channel=method.routing_key):
+                        # Try to dispatch messages as soon as they're generated,
+                        # but store them for later if the connection is dropped
+                        self._pending.append((routing_key, message))
+                        if not failed:
+                            try:
+                                # Request a call to dispatch_pending as soon as possible in the context
+                                # of the connection’s thread
+                                dispatch_callback = functools.partial(self.dispatch_pending, expected=1)
+                                connection.add_callback_threadsafe(dispatch_callback)
+                            except RECOVERABLE_PIKA_ERRORS:
+                                failed = True
+            except:
+                pass
+
+            # Request a call to ack_message as soon as possible in the context of the connection’s thread
+            ack_callback = functools.partial(_ack_message, channel, method.delivery_tag)
+            connection.add_callback_threadsafe(ack_callback)
+
+        def _on_message(channel, method, properties, body, args):
+            """ Creates a new thread for handling the message, and launches the actual job in it """
+            (connection, threads) = args
+            thread = threading.Thread(target=_queue_callback, args=(connection, channel, method, body))
+            thread.start()
+            threads.append(thread)
+
+        logger.debug("main thread: %d" % threading.get_ident())
 
         while True:
             consumer_tags = []
+            worker_threads = []
             try:
                 for queue in self._read:
+                    on_message_callback = functools.partial(_on_message, args=(self.connection, worker_threads))
                     consumer_tags.append(
-                            self.channel.basic_consume(queue, _queue_callback))
+                            self.channel.basic_consume(queue, on_message_callback))
                 self.dispatch_pending(expected=0)
                 self.channel.start_consuming()
             except RECOVERABLE_PIKA_ERRORS:
@@ -176,6 +219,8 @@ class PikaPipelineRunner(PikaConnectionHolder):
                 for tag in consumer_tags:
                     self.channel.basic_cancel(tag)
                 self.channel.stop_consuming()
+                for t in worker_threads:
+                    t.join()
                 raise
 
 
